c9a58bc6bb88231ccddcd86a4b3c339c150c3b03
Marius Gerold <marius.gerold@hs-furtwangen.de>
2016-11-22 17:11:16 +0100

    src und pom

---
+++ src/main/java/de/marius/Großbuchstaben.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Großbuchstaben {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String eingabe = scanner.nextLine();
+		System.out.println(eingabe.toUpperCase());
+
+	}
+
+}
